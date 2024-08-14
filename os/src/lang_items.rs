672513f3abe3e05eadca1ed282160a725691fa58
reganzm <626692024@qq.com>
2024-08-14 16:04:19 +0800

    [add]bare machine surpport

---
+++ os/src/lang_items.rs
@@ -0,0 +1,8 @@
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info:&PanicInfo)->!{
+    loop {
+        
+    }
+}
---
+++ os/src/main.rs
@@ -0,0 +1,8 @@
+// tell compiler there is no main fn
+#![no_main]
+// tell compiler there is no std libs
+#![no_std]
+
+
+mod lang_items;
+
