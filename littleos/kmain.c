f553fe8dbd30e768356177267f84c9b195ebd406
reganzm <626692024@qq.com>
2024-08-21 16:47:32 +0800

    [add]use make tool to manage the project

--- littleos/kmain.c
+++ littleos/kmain.c
@@ -1,5 +1,12 @@
+
+
 struct example{
     unsigned char config;
     unsigned short address;
     unsigned char index;
 }__attribute__((packed));
+
+
+int sum_of_three(int arg1,int arg2,int arg3){
+    return arg1+arg2+arg3;
+}
